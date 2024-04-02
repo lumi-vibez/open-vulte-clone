0bbb225bf7b4f14cfe2ea89ae0146084ca7dd776
dahmaular <dahmaular@gmail.com>
2024-04-02 15:14:25 +0100

    feature: responsive design, hamburger

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log('connected');
+
+// console.dir(window);
+
+const nav = document.getElementById('nav');
+// console.log(nav);
+
+const body = document.getElementById('body');
+// console.log(body);
+
+
+const navContainer = document.getElementById('hidden');
+// console.log(navContainer);
+// console.dir(navContainer)
+
+
+function createNavOnScroll() {
+    navContainer.style.backgroundColor = "#fff";
+    navContainer.style.position = "sticky";
+    navContainer.style.top  = 0;
+    navContainer.style.transition = 'all easeOut 2s';
+    navContainer.style.borderBottom = '1px solid #eee';
+    // navContainer.style.border = '2px solid red';
+}
+
+
+window.addEventListener('scroll', () => {
+    console.log('this is the window scrollY property in px unit.' ,window.scrollY + 'px')
+
+        if(window.scrollY >= 80) {
+            createNavOnScroll();
+         }
+
+         else {
+          navContainer.style.background = 'transparent';
+         }
+
+          // while (window.scrollY >= 80) {
+          //   createNavOnScroll()
+          // } 
+
+
+})
+
