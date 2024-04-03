d75d09ed93d16faf715b668f1edf899903852baf
dahmaular <dahmaular@gmail.com>
2024-04-03 15:06:42 +0100

    feature: responsive navbar

--- script.js
+++ script.js
@@ -14,31 +14,46 @@ const navContainer = document.getElementById('hidden');
 // console.dir(navContainer)
 
 
-function createNavOnScroll() {
-    navContainer.style.backgroundColor = "#fff";
-    navContainer.style.position = "sticky";
-    navContainer.style.top  = 0;
-    navContainer.style.transition = 'all easeOut 2s';
-    navContainer.style.borderBottom = '1px solid #eee';
-    // navContainer.style.border = '2px solid red';
-}
+const hamburger = document.querySelector('.svg-container')
+console.log('Hamburger element' ,hamburger);
 
+const dropDown = document.getElementById('dropdown');
+console.dir('This is the dropDown element' ,dropDown)
 
-window.addEventListener('scroll', () => {
-    console.log('this is the window scrollY property in px unit.' ,window.scrollY + 'px')
-
-        if(window.scrollY >= 80) {
-            createNavOnScroll();
-         }
-
-         else {
-          navContainer.style.background = 'transparent';
-         }
-
-          // while (window.scrollY >= 80) {
-          //   createNavOnScroll()
-          // } 
-
-
+hamburger.addEventListener('click', () => {
+    dropDown.classList.toggle('mobile-menu-container')
 })
+
+
+
+
+
+
+// function createNavOnScroll() {
+//     navContainer.style.backgroundColor = "#fff";
+//     navContainer.style.position = "sticky";
+//     navContainer.style.top  = 0;
+//     navContainer.style.transition = 'all easeOut 2s';
+//     navContainer.style.borderBottom = '1px solid #eee';
+//     // navContainer.style.border = '2px solid red';
+// }
+
+
+// window.addEventListener('scroll', () => {
+//     console.log('this is the window scrollY property in px unit.' ,window.scrollY + 'px')
+
+//         if(window.scrollY >= 80) {
+//             createNavOnScroll();
+//          }
+
+//          else {
+//           navContainer.style.background = 'transparent';
+//          }
+
+//           // while (window.scrollY >= 80) {
+//           //   createNavOnScroll()
+//           // } 
+
+
+// })
 
